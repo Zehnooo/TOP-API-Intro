f2f3887a4d7010ba63551ad4ee4be55be9bfd635
Zehno <126801501+Zehnooo@users.noreply.github.com>
2026-07-20 14:52:23 -0500

    added giphy attribute, added input placeholder and updated styles for input

--- dom.js
+++ dom.js
@@ -2,6 +2,7 @@ import {findGif} from './tools.js'
 
 
 export const dom = () => {
+    const  main  = document.querySelector('#root');
 
     const con = document.createElement('div');
         con.className = 'container';
@@ -16,6 +17,7 @@ export const dom = () => {
 
     const search = document.createElement('input');
         search.addEventListener('input', () => findGif());
+        search.placeholder  = 'Cats and dogs';
 
     const newGif = document.createElement('button');
         newGif.innerHTML = '<svg width="18px" height="18px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M4.06189 13C4.02104 12.6724 4 12.3387 4 12C4 7.58172 7.58172 4 12 4C14.5006 4 16.7332 5.14727 18.2002 6.94416M19.9381 11C19.979 11.3276 20 11.6613 20 12C20 16.4183 16.4183 20 12 20C9.61061 20 7.46589 18.9525 6 17.2916M9 17H6V17.2916M18.2002 4V6.94416M18.2002 6.94416V6.99993L15.2002 7M6 20V17.2916" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>';
@@ -28,16 +30,26 @@ export const dom = () => {
     const gif = document.createElement('img');
         gif.id = 'gif-slot';
 
+    const gifyAttribute  = document.createElement('img');
+    gifyAttribute.src = './giphy.png';
+
+    const  footer  = document.createElement('footer');
+    const footerCon  =  document.createElement('div');
+    const github =  document.createElement('a');
+    github.href  = 'https://github.com/Zehnooo/TOP-API-Intro';
+
+
+
     const toastCon = document.createElement('div');
     toastCon.className = 'toasts';
 
     // document.body.append(toastCon);
-    imgCon.append(gif);
+    imgCon.append(gif, gifyAttribute);
     inputCon.append(search, newGif);
     content.append(inputCon, imgCon);
     con.append(head, content, toastCon);
-
-    return con;
+    main.append(con);
+    return main;
 }
 
 export function createToast(msg, type) {
